804aa4e75b3686579e4ac82ed3031dd503f2a3ad
Pradip Bade <badepradip123@gmail.com>
2026-09-25 01:29:17 +0530

    fix: configure cors origins

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import compression from 'compression';
+import * as compression from 'compression';
 import helmet from 'helmet';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
